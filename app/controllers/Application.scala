9214d97080a425fb2eb36d14aecabbbfaeb522c3
Toby Sullivan <tobyjsullivan@gmail.com>
2015-01-04 01:02:35 -0800

    Implement event index endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
   val svcStudio = Play.configuration.getString("service.studio.hostname").getOrElse(throw new Exception("Failed to load config"))
+  val svcEvent = Play.configuration.getString("service.event.hostname").getOrElse(throw new Exception("Failed to load config"))
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -16,6 +17,13 @@ object Application extends Controller {
 
   def indexStudios = Action.async {
     val holder : WSRequestHolder = WS.url(svcStudio + "/studios")
+    holder.get().map { response =>
+      Ok(response.json)
+    }
+  }
+
+  def indexEvents = Action.async {
+    val holder : WSRequestHolder = WS.url(svcEvent + "/events")
     holder.get().map { response =>
       Ok(response.json)
     }
