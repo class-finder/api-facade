a5aaa373fb36f96a1f43fbf74c44aed6377ca169
Toby Sullivan <tobyjsullivan@gmail.com>
2015-01-04 00:51:24 -0800

    Implement studio index endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,23 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
+import play.api.libs.ws._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
+  val svcStudio = Play.configuration.getString("service.studio.hostname").getOrElse(throw new Exception("Failed to load config"))
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def indexStudios = Action.async {
+    val holder : WSRequestHolder = WS.url(svcStudio + "/studios")
+    holder.get().map { response =>
+      Ok(response.json)
+    }
   }
 
 }
